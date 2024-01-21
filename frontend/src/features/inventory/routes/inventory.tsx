8267d6c0889191fe1df9704b5850bb602a803e38
Joe Luensmann <jtluensmann@gmail.com>
2024-01-21 17:46:31 -0500

    Rewrote the offcanvas to use nested navs

--- frontend/src/features/inventory/routes/inventory.tsx
+++ frontend/src/features/inventory/routes/inventory.tsx
@@ -4,17 +4,21 @@ import { Compass } from "react-bootstrap-icons";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import { ListGroup, Offcanvas, Accordion, Container } from "react-bootstrap";
+import { Offcanvas, Accordion, Container, Collapse } from "react-bootstrap";
 
 export function Inventory() {
   const [show, setShow] = useState(false);
+  const [open, setOpen] = useState(true);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
     <>
-      <Row className="justify-content-md-center text-center" data-bs-theme="dark">
+      <Row
+        className="justify-content-md-center text-center pt-4"
+        data-bs-theme="dark"
+      >
         <Col xs lg={8}>
           <Accordion defaultActiveKey="1">
             <Accordion.Item eventKey="1">
@@ -24,7 +28,10 @@ export function Inventory() {
                   {Array.from({ length: 4 }).map((_, idx) => (
                     <Col key={idx}>
                       <Card className="shadow-lg bg-body-tertiary">
-                        <Card.Img variant="top" src="https://placehold.co/100" />
+                        <Card.Img
+                          variant="top"
+                          src="https://placehold.co/100"
+                        />
                         <Card.Body>
                           <Card.Title>Container</Card.Title>
                         </Card.Body>
@@ -38,13 +45,12 @@ export function Inventory() {
               <Accordion.Header>Items</Accordion.Header>
             </Accordion.Item>
           </Accordion>
-          
         </Col>
       </Row>
 
       <button
-            title="Open Navigation"
-            className="tw-fixed 
+        title="Open Navigation"
+        className="tw-fixed 
             tw-z-90 
             tw-bottom-10 
             tw-left-8 
@@ -61,10 +67,10 @@ export function Inventory() {
             hover:tw-drop-shadow-2xl 
             hover:tw-animate-bounce 
             duration-10"
-            onClick={handleShow}
-          >
-            <Compass />
-          </button>
+        onClick={handleShow}
+      >
+        <Compass />
+      </button>
 
       <Offcanvas show={show} onHide={handleClose} data-bs-theme="dark">
         <Offcanvas.Header closeButton>
@@ -74,18 +80,29 @@ export function Inventory() {
           <Container>
             <Row>
               <Col>
-                <Accordion defaultActiveKey="0">
-                  <Accordion.Item eventKey="0">
-                    <Accordion.Header>Home</Accordion.Header>
-                    <Accordion.Body>
-                      <ListGroup>
-                        <ListGroup.Item>Living Room</ListGroup.Item>
-                        <ListGroup.Item>Dining Room</ListGroup.Item>
-                        <ListGroup.Item>Kitchen</ListGroup.Item>
-                      </ListGroup>
-                    </Accordion.Body>
-                  </Accordion.Item>
-                </Accordion>
+                <nav className="h-100 flex-column align-items-stretch pe-4 border-end">
+                  <nav className="nav nav-pills flex-column">
+                    <button
+                      className="nav"
+                      onClick={() => setOpen(!open)}
+                      aria-controls="collapse-tree"
+                      aria-expanded={open}
+                    >
+                      Home
+                    </button>
+                    <Collapse in={open}>
+                      <div id="collapse-tree">
+                        <nav className="nav nav-pills flex-column">
+                          <a className="nav-link ms-3 my-1">Living Room</a>
+                          <a className="nav-link ms-3 my-1">Dining Room</a>
+                          <a className="nav-link ms-3 my-1">Kitchen</a>
+                          <a className="nav-link ms-3 my-1">Office</a>
+                          <a className="nav-link ms-3 my-1">Bedroom</a>
+                        </nav>
+                      </div>
+                    </Collapse>
+                  </nav>
+                </nav>
               </Col>
             </Row>
           </Container>
--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -7,9 +7,14 @@ import { Outlet } from "react-router-dom";
 
 function Root() {
   return (
-    <Container fluid className="tw-bg-slate-800 tw-text-slate-200 m-0 p-0">
+    <Container
+      fluid
+      className="tw-bg-slate-800 tw-text-slate-200 m-0 p-0 tw-min-h-screen"
+    >
       <Navigation />
-      <Outlet />
+      <Container className="tw-bg-slate-800 tw-min-h-screen tw-min-w-full">
+        <Outlet />
+      </Container>
     </Container>
   );
 }
