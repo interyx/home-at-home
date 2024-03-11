518d3f75f5e3ec885910d1fb199661d956233d24
interyx <jluensma@asu.edu>
2024-03-11 12:49:13 -0400

    Server refactoring wired correctly!

---
+++ backend/src/server/server.ts
@@ -0,0 +1,18 @@
+import "./loadEnvironment"
+const mongoose = require('mongoose')
+const app = require('../app')
+const port = process.env.PORT || 4000;
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb://localhost:27017"
+    )
+    app.listen(port, () => console.log(`Server started on port ${port}`))
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+start();
